8c1dc22e1b1ac4d010dd1c21a71e2e1d65f9ed2f
Stephanie Chang <schang1@godaddy.com>
2023-07-08 19:31:42 -0600

    removing weird jquery logs

--- docs/scripts/extras.js
+++ docs/scripts/extras.js
@@ -1,4 +1,4 @@
 $(document).ready (function(){
- $('#about_pic').tooltip({ position: {my:"center+40 center+40", at:"left-10 center+10"}, show: { effect: "fade", duration: 400, selector: true } });
- $('[title]').tooltip({ show: { effect: "fade", duration: 400, selector: true } });
+    $('#about_pic').tooltip({ position: {my:"center+40 center+40", at:"left-10 center+10"}, show: { effect: "fade", duration: 400, selector: true } });
+     $('[title]').tooltip({ show: { effect: "fade", duration: 400, selector: true }});
 });
